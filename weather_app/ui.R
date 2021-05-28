4e089912e9cd444e521bd39d16b2b645cdd6a505
Nybre <g11m1065trader@gmail.com>
2021-05-28 18:04:31 +0200

    Update ui.R

--- weather_app/ui.R
+++ weather_app/ui.R
@@ -1,12 +1,15 @@
+#used the semantic.shiny library as it has a nicer feel
 shinyUI(
   dashboardPage( 
-    dashboard_header(show_menu_button = T  ,titleWidth = "wide",logo_align = "center" , inverted = F,
-                      
+    #Dashboard header
+    dashboard_header(show_menu_button = T  ,titleWidth = "wide",logo_align = "center" , inverted = F, 
                      title= "Netherlands Weather App"),  
+    #inactive as theres only one output
     dashboardSidebar(side = "left",disable = TRUE,
                      sidebarMenu( 
                        menuItem("Dashboard", tabName = "dash", icon = icon("info",lib = "font-awesome")) 
                      )),  
+    #Body of the dashboard sourcing the UI component of the dashboard_file.R script
     dashboardBody(    
       tabItems(   
         tabItem(tabName = "dash",   
