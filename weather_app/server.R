a96d659fe7d2c8e52a5a2114bec832ddd7f5fa60
Nybre <g11m1065trader@gmail.com>
2021-05-28 18:04:50 +0200

    Update server.R

--- weather_app/server.R
+++ weather_app/server.R
@@ -3,6 +3,8 @@ options(digits.secs = 3) # Include milliseconds in time display
 shinyServer(function(input, output,session) {
   
   #----------------server modules------------------
+  
+  #server component sourced from dashboard_file.R server side
   callModule(Dashboard_file, "Dashboard_file-module", pool) 
   
 })
